00a08682c741cc31175f989740f91dd3777fd72c
unknown <under.dark.night@outlook.com>
2024-06-26 01:37:57 +0800

    console blocker finish

--- key_modules_of_funny_cat/06_players/02_anti_cheating_module.lua
+++ key_modules_of_funny_cat/06_players/02_anti_cheating_module.lua
@@ -4,28 +4,28 @@
 
     consolescreen.lua
 
+            ----------------------------------------------------------------------------------
+            --- 关掉控制台的界面push
+                -- inst:DoTaskInTime(3,function()
+                --     if TheFrontEnd then
+                --         local old_PushScreen = TheFrontEnd.PushScreen
+                --         TheFrontEnd.PushScreen = function(self,screen,...)
+                --             -- print(screen.name)
+                --             if screen.name == "ConsoleScreen" then
+                --                 screen:OnDestroy()
+                --                 return
+                --             end
+                --             return old_PushScreen(self,screen,...)
+                --         end
+                --     end
+                -- end)
+            ----------------------------------------------------------------------------------
+
 ]]--
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --- 控制台关闭
     if not TUNING.FUNNY_CAT_DEBUGGING_MODE then
         AddPlayerPostInit(function(inst)
-
-            ----------------------------------------------------------------------------------
-            --- 关掉控制台的界面push
-                inst:DoTaskInTime(3,function()
-                    if TheFrontEnd then
-                        local old_PushScreen = TheFrontEnd.PushScreen
-                        TheFrontEnd.PushScreen = function(self,screen,...)
-                            -- print(screen.name)
-                            if screen.name == "ConsoleScreen" then
-                                screen:OnDestroy()
-                                return
-                            end
-                            return old_PushScreen(self,screen,...)
-                        end
-                    end
-                end)
-            ----------------------------------------------------------------------------------
 
             if not TheWorld.ismastersim then
                 return
@@ -35,7 +35,19 @@
             local code_in_client_side = [[
                 ThePlayer.replica.funny_cat_com_safe_sys:PushEvent("console_closed")
                 CONSOLE_ENABLED = false
-                print("+++++ info +++++++",ThePlayer,ThePlayer.replica.funny_cat_com_safe_sys)
+                local old_PushScreen = nil
+                if TheFrontEnd then
+                    old_PushScreen = TheFrontEnd.PushScreen
+                    TheFrontEnd.PushScreen = function(self,screen,...)
+                        -- print(screen.name)
+                        if screen.name == "ConsoleScreen" then
+                            screen:OnDestroy()
+                            return
+                        end
+                        return old_PushScreen(self,screen,...)
+                    end
+                end
+                print("+++++ info console block +++++++",old_PushScreen)
             ]]
 
             local tempInst = CreateEntity()            
